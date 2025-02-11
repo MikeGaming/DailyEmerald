309c58b1441ff1baf66a0977718b91424bac7611
Lucas Abramczuk <lucas.abramczuk@ontariotechu.net>
2025-02-10 19:11:35 -0500

    sword
    works

--- Assets/LucasTest/SwordBlade.cs
+++ Assets/LucasTest/SwordBlade.cs
@@ -1,24 +1,37 @@
 using UnityEngine;
 
+[System.Serializable]
 public class SwordBlade : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private Transform hitPoint;
+    [SerializeField]
+    private GameObject swordFull;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private int hitCount;
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Hammer")
         {
+            Collider[] cols = Physics.OverlapSphere(hitPoint.position, 5f);
+            if(cols.Length > 0f)
+            {
+                Debug.Log("pears"); 
+                foreach (Collider col in cols)
+                {
+                    if(col.tag == "Handle")
+                    {
+                        Instantiate(swordFull, transform.position + new Vector3(0, 0.25f, 0), Quaternion.Euler(0, 0, 0));
+                        //would be neat to sort by distance and destroy the closest
+                        //I have a custom sorter for distance already I can port over
+                        Destroy(col.transform.parent.gameObject);
+                        Destroy(this.gameObject);
 
+                        return;
+                    }
+                }
+            }
         }
     }
 }
