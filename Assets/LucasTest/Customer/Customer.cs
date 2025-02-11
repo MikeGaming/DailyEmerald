7f793f0fd47dad6b15183588fdeae4147de93349
Lucas Abramczuk <lucas.abramczuk@ontariotechu.net>
2025-02-11 15:18:49 -0500

    cudtom
    i love buying

---
+++ Assets/LucasTest/Customer/Customer.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Customer : MonoBehaviour
+{
+    [SerializeField]
+    private string[] itemList;
+    [SerializeField]
+    private string[] magicList;
+    [SerializeField]
+    private Sprite[] imageList;
+
+    [SerializeField]
+    private GameObject canvasObj;
+    [SerializeField]
+    private Image imageObj;
+
+    private int orderIndex;
+    private string wantedMagic;
+
+    private void Start()
+    {
+        Order();
+    }
+
+    private void Order()
+    {
+        Debug.Log(itemList.Length);
+        orderIndex = Random.Range(0, itemList.Length);
+        imageObj.sprite = imageList[orderIndex];
+        canvasObj.SetActive(true);
+        wantedMagic = magicList[Random.Range(0, magicList.Length)];
+    }
+
+    public string[] GetOrder()
+    {
+        return new string[] {itemList[orderIndex], wantedMagic};
+    }
+
+    public void ServeCustomer()
+    {
+        Debug.Log("Served!");
+    }
+}
---
+++ Assets/LucasTest/Customer/ShopItem.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+[System.Serializable]
+public class ShopItem : MonoBehaviour
+{
+    [SerializeField]
+    private string objName;
+    private string magic;
+
+    public void SetMagic(string mag)
+    {
+        magic = mag;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Customer")
+        {
+            string[] custOrder = other.GetComponent<Customer>().GetOrder();
+            if(objName == custOrder[0] && magic == custOrder[1])
+            {
+                other.GetComponent<Customer>().ServeCustomer();
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
