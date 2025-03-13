ff005a7e766bdae4327faa859a375422620c6f77
Mikey <quecanomichael@gmail.com>
2025-03-13 18:30:31 -0400

    colours fixed, stone sound in

--- Assets/Scripts/HammerParticles.cs
+++ Assets/Scripts/HammerParticles.cs
@@ -4,11 +4,13 @@ using UnityEngine.XR.Interaction.Toolkit.Interactables;
 public class HammerParticles : MonoBehaviour
 {
     [SerializeField] XRGrabInteractable hammer;
+    FMODUnity.StudioEventEmitter emitter;
     ParticleSystem particle;
 
     private void Start()
     {
         particle = GetComponent<ParticleSystem>();
+        emitter = GetComponent<FMODUnity.StudioEventEmitter>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -16,7 +18,43 @@ public class HammerParticles : MonoBehaviour
         if (hammer.isGrabbed)
         {
             particle.Emit(50);
+            emitter.Play();
             Debug.Log("pomegranate");
+        }
+
+        switch (other.tag)
+        {
+            case "Metal":
+            case "Blade":
+                emitter.SetParameter("HammeredMaterial", 0);
+                break;
+
+            case "Wood":
+            case "Handle":
+                emitter.SetParameter("HammeredMaterial", 1);
+                break;
+
+            case "Stone":
+            case "Oven":
+                emitter.SetParameter("HammeredMaterial", 2);
+                break;
+
+            case "Player":
+                emitter.SetParameter("HammeredMaterial", 3);
+                break;
+
+            case "Customer":
+                emitter.SetParameter("HammeredMaterial", 4);
+                break;
+
+            case "Crystal":
+            case "CrystalBit":
+                emitter.SetParameter("HammeredMaterial", 5);
+                break;
+
+            case "Money":
+                emitter.SetParameter("HammeredMaterial", 6);
+                break;
         }
     }
 }
