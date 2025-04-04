6199468d8e32bd6a6545906447e6bbb85153b554
Lucas Abramczuk <lucas.abramczuk@ontariotechu.net>
2025-04-03 23:43:39 -0400

    Order Updates
    Updated order text
    Also fixed the random seeder

--- Assets/Scripts/MainMenu/Seeder.cs
+++ Assets/Scripts/MainMenu/Seeder.cs
@@ -4,15 +4,17 @@ using System.Linq;
 public class Seeder : MonoBehaviour
 {
     [SerializeField] private Transform checkLoc;
+    [SerializeField] private LayerMask ignoreLayer;
 
     public void SetSeed()
     {
-        Collider[] objs = Physics.OverlapSphere(checkLoc.position, 0.5f);
+        Collider[] objs = Physics.OverlapSphere(checkLoc.position, 0.5f, Physics.AllLayers & ~(1 << ignoreLayer));
         objs = objs.OrderBy((x) => (x.transform.position - checkLoc.position).sqrMagnitude).ToArray();
 
-        int seed = 0;
+        int seed = Mathf.FloorToInt(Time.realtimeSinceStartup);
         if (objs.Length > 0)
         {
+            seed = 0;
             for (int i = 0; i < objs[0].name.Length; i++)
             {
                 byte temp = (byte)objs[0].name[i];
